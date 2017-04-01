89d05ff11fd9660ff8a384f2b1f0430f2c837ea3
rg3915 <rg3915@yahoo.com.br>
2017-04-01 12:13:40 -0300

    Static on core app.

---
+++ djfront/core/static/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('form').submit(function(event) {
+    event.preventDefault();
+    // console.log(this.data('urlredirect')); // TypeError: this.data is not a function
+    console.log($(this).attr('data-urlredirect'));
+    urlredirect = $(this).data('urlredirect');
+    console.log(urlredirect);
+    $.ajax({
+      type: 'POST',
+      url: this.action, // this nativo do JS
+      data: $(this).serialize(),
+      dataType: 'json',
+      success: function(data){
+        console.log(data);
+        location.href = urlredirect;
+      },
+      error: function(error, response, settings){
+        console.error(settings.url, response, error.toString());
+      }
+    });
+  });
+});
