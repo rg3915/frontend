3a705f942d3a0ce2677af52350f63e186ed963f3
rg3915 <rg3915@yahoo.com.br>
2017-04-01 11:32:23 -0300

    Refatorando urls

--- djfront/urls.py
+++ djfront/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('djfront.djbasic.urls', namespace='core')),
+    url(r'', include('djfront.djbasic.urls', namespace='djbasic')),
     url(r'^admin/', admin.site.urls),
 ]
