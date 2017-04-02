6269b7f739ec8c3428f99d437756e48c9c190bc8
rg3915 <rg3915@yahoo.com.br>
2017-04-02 03:25:03 -0300

    random colors

---
+++ static/js/random-colors.js
@@ -0,0 +1,16 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+}
+
+$('.color_me').css("background-color", getRandomColor); 
+$('.color_me_hover').text(getRandomColor); 
+
+$('.color_me').click(function() {
+  $(this).css("background-color", getRandomColor); 
+  $(this).children().text(getRandomColor); 
+});
