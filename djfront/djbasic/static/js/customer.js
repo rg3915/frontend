c456d50b74633ee216a9f389237d977620c3ee89
rg3915 <rg3915@yahoo.com.br>
2017-04-21 22:30:09 -0300

    Refatorando customer.js

--- djfront/djbasic/static/js/customer.js
+++ djfront/djbasic/static/js/customer.js
@@ -1,22 +1,20 @@
-$(document).ready(function(){
-  $('form').submit(function(e) {
-    e.preventDefault();
-    // console.log(this.data('urlredirect')); // TypeError: this.data is not a function
-    console.log($(this).attr('data-urlredirect'));
-    urlredirect = $(this).data('urlredirect');
-    console.log(urlredirect);
-    $.ajax({
-      type: 'POST',
-      url: this.action, // this nativo do JS
-      data: $(this).serialize(),
-      dataType: 'json',
-      success: function(data){
-        console.log(data);
-        location.href = urlredirect;
-      },
-      error: function(error, response, settings){
-        console.error(settings.url, response, error.toString());
-      }
-    });
+$('form').submit(function(e) {
+  e.preventDefault();
+  // console.log(this.data('urlredirect')); // TypeError: this.data is not a function
+  console.log($(this).attr('data-urlredirect'));
+  urlredirect = $(this).data('urlredirect');
+  console.log(urlredirect);
+  $.ajax({
+    type: 'POST',
+    url: this.action, // this nativo do JS
+    data: $(this).serialize(),
+    dataType: 'json',
+    success: function(data){
+      console.log(data);
+      location.href = urlredirect;
+    },
+    error: function(error, response, settings){
+      console.error(settings.url, response, error.toString());
+    }
   });
 });
